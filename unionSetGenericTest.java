3d592cf969c64fd713627707cc5497bd114566c9
Cristina Gallego <crismgh23@gmail.com>
2017-12-08 11:36:43 +0100

    primer test SetGeneric

--- unionSetGenericTest.java
+++ unionSetGenericTest.java
@@ -1,12 +1,25 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.*;
 
 public class unionSetGenericTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Set<Integer> vect1 = new HashSet<>();
+	Set<Integer> vect2 = new HashSet<>();
+	
+	@Before public void setUp()         // Preparamos los parametros para realizar los test
+	{     
+		vect1.add(1);
+		vect2.add(2);
+		vect2.add(3);
+	}
+	
+	@Test(expected=NullPointerException.class) 
+	public void VectorsAreNull()
+	{
+		Union.unionSetGeneric(null, null);
 	}
 
 }

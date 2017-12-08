249f2e46379f9a3e18abb040e39159dcc3af1012
Cristina Gallego <crismgh23@gmail.com>
2017-12-08 11:05:19 +0100

    creamso el nuevo test para SET

---
+++ UnionSetTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.Vector;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class UnionSetTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

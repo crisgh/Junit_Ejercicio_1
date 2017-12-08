7c002fe859e270f269c0748e7eb85c7b8abbdc50
Cristina Gallego <crismgh23@gmail.com>
2017-12-08 11:10:31 +0100

    primer test para UnionSet

--- UnionSetTest.java
+++ UnionSetTest.java
@@ -1,17 +1,26 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-
+import org.junit.*;
 import java.util.Vector;
 import java.util.HashSet;
 import java.util.Set;
 
 
-public class UnionSetTest {
+public class UnionSetTest{
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Set<Integer> vect1 = new HashSet<>();
+	Set<Integer> vect2 = new HashSet<>();
+	
+	@Before public void setUp()         // Preparamos los parametros para realizar los test
+	{     
+		vect1.add(1);
+		vect2.add(2);
+		vect2.add(3);
 	}
-
+	
+	@Test(expected=NullPointerException.class) 
+	public void VectorsAreNull()
+	{
+		Union.unionSet(null, null);
+	}
 }
